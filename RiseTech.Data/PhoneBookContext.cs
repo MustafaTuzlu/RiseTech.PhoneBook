ad885aee7d7a2711501858fca36d4c76d24cdbd1
Mustafa TUZLU <tuzlumustafa@gmail.com>
2022-11-28 13:07:57 +0300

    Test Project added

--- RiseTech.Data/PhoneBookContext.cs
+++ RiseTech.Data/PhoneBookContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using RiseTech.Data.Entities;
+using System.IO;
 
 namespace RiseTech.Data
 {
@@ -9,5 +11,14 @@ namespace RiseTech.Data
         public DbSet<Person> People { get; set; }
         public DbSet<Info> Info { get; set; }
         public DbSet<Report> Reports { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=ContactDb;User Id=postgres;Password=7F4df451mt!;");
+            }
+            base.OnConfiguring(optionsBuilder); 
+        }
     }
 }
